e99c93e388d1fda28cd8d6b71f1fdb3a7c227f34
Patnjop <36369847+Patnjop@users.noreply.github.com>
2019-05-14 18:05:17 +0100

    Toolbar finished and basic unit placement

--- Assets/ToolbarExpansion.cs
+++ Assets/ToolbarExpansion.cs
@@ -6,13 +6,15 @@ using UnityEngine.UI;
 public class ToolbarExpansion : MonoBehaviour
 {
     public Button unitToolbarButton;
+    public GameObject[] unitTypes;
     public Image Background;
     RectTransform unitToolbar;
     public Text unitArrow, unitArrow2;
-    public bool expand = false;
-    public int maxWidth;
+    public bool expand, chosen = false;
+    public int maxWidth, unitSelected, unitSpeed;
     Vector2 startWidth;
     RectTransform unitButtonTransform;
+    public UnitPlacement unitPlacement;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,31 +27,59 @@ public class ToolbarExpansion : MonoBehaviour
     void SwitchToolbar()
     {
         if (expand == false)
-        {   
+        {
             unitArrow.enabled = false;
+            StartCoroutine("Appear");
             expand = true;
         }
         else if (expand == true)
         {
             unitArrow2.enabled = false;
+            StartCoroutine("Disappear");
             expand = false;
         }
     }
 
     void Update()
     {
+        if (unitSelected == 0)
+        {
+            unitSpeed = 0;
+        }
+        if (unitSelected >= 1)
+        {
+            unitSpeed = (unitSelected * 80);
+        }
+
+
         if (expand == true && unitToolbar.sizeDelta.x <= maxWidth)
         {
             unitToolbar.position = unitToolbar.position + new Vector3(Time.deltaTime * 150f, 0, 0);
             unitToolbarButton.GetComponent<RectTransform>().position = unitToolbarButton.GetComponent<RectTransform>().position + new Vector3(Time.deltaTime * 300f, 0, 0);
             unitToolbar.sizeDelta = unitToolbar.sizeDelta + new Vector2(Time.deltaTime * 300f, 0);
+            for (int i = 0; i < unitTypes.Length; i++)
+            {
+                if (unitTypes[i].GetComponent<RectTransform>().position.x <= 35 + (i * 70))
+                {
+                    unitTypes[i].GetComponent<RectTransform>().position = unitTypes[i].GetComponent<RectTransform>().position + new Vector3(Time.deltaTime * i * 148, 0, 0);
+                }
+            }
         }
         if (expand == false && unitToolbar.sizeDelta.x > startWidth.x)
         {
             unitToolbar.position = unitToolbar.position - new Vector3(Time.deltaTime * 150f, 0, 0);
             unitToolbarButton.GetComponent<RectTransform>().position = unitToolbarButton.GetComponent<RectTransform>().position - new Vector3(Time.deltaTime * 300f, 0, 0);
             unitToolbar.sizeDelta = unitToolbar.sizeDelta - new Vector2(Time.deltaTime * 300f, 0);
+            for (int g = 0; g < unitTypes.Length; g++)
+            {
+                if (unitTypes[g].GetComponent<RectTransform>().position.x > 35)
+                {
+                    unitTypes[g].GetComponent<RectTransform>().position = unitTypes[g].GetComponent<RectTransform>().position - new Vector3(Time.deltaTime * g * 148, 0, 0);
+                }
+            }
         }
+
+
         if (unitToolbar.sizeDelta.x >= maxWidth)
         {
             unitArrow2.enabled = true;
@@ -57,6 +87,48 @@ public class ToolbarExpansion : MonoBehaviour
         if (unitToolbar.sizeDelta.x <= startWidth.x)
         {
             unitArrow.enabled = true;
+        }
+
+        if (expand == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                unitSelected = 0;
+                SwitchToolbar();
+                unitPlacement.SetUnit(unitPlacement.units[unitSelected]);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                unitSelected = 1;
+                unitPlacement.SetUnit(unitPlacement.units[unitSelected]);
+                SwitchToolbar();
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                unitSelected = 2;
+                SwitchToolbar();
+            }
+        }
+    }
+    IEnumerator Appear()
+    {
+        for (int a = 0; a < unitTypes.Length; a++)
+        {
+            yield return new WaitForSeconds(0.1f * a);
+            unitTypes[a].GetComponent<Image>().enabled = true;
+            unitTypes[a].GetComponent<Button>().enabled = true;
+        }
+    }
+    IEnumerator Disappear()
+    {
+        for (int a = 2; a > -1; a--)
+        {
+            if (a != unitSelected)
+            {
+                yield return new WaitForSeconds(0.1f * a);
+                unitTypes[a].GetComponent<Image>().enabled = false;
+                unitTypes[a].GetComponent<Button>().enabled = false;
+            }
         }
     }
 }
---
+++ Assets/UnitPlacement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitPlacement : MonoBehaviour
+{
+    public GameObject[] units;
+    public Material BlueInfantryMat;
+    public GameObject UIManager;
+    ToolbarExpansion toolbarExpansion;
+
+    bool unitPlaced;
+    int classIndex;
+    Transform currentUnit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toolbarExpansion = UIManager.GetComponent<ToolbarExpansion>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        classIndex = toolbarExpansion.unitSelected;
+        if (currentUnit != null && !unitPlaced)
+        {
+            Vector3 m = Input.mousePosition;
+            m = new Vector3(m.x, m.y, 9);
+            Vector3 p = Camera.main.ScreenToWorldPoint(m);
+            currentUnit.position = new Vector3(p.x, 0.1f, p.z);
+            if (Input.GetMouseButtonDown(0))
+            {
+                unitPlaced = true;
+                SetUnit(units[toolbarExpansion.unitSelected]);
+            }
+        }
+    }
+    public void SetUnit(GameObject g)
+    {
+        unitPlaced = false;
+        currentUnit = ((GameObject)Instantiate(g)).transform;
+    }
+}
+
+
+
