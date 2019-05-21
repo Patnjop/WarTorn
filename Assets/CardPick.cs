19cd5925284483abb6c89d21a01645ac57ad1d34
Patnjop <36369847+Patnjop@users.noreply.github.com>
2019-05-21 13:54:58 +0100

    Deckbuilding

---
+++ Assets/CardPick.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPick : MonoBehaviour
+{
+    public GameObject[] cards;
+    public int choiceCount;
+    public int cardAmount;
+    public int xWidth;
+    bool canChoose = true;
+    List<GameObject> activeCards = new List<GameObject>();
+    public List<int> deck = new List<int>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (choiceCount > 0 && canChoose == true)
+        {
+            InitialCardChoice();
+            canChoose = false;
+        }
+    }
+
+    void InitialCardChoice()
+    {
+        for (int i = 0; i < cardAmount; i++)
+        {
+            int rnd = Random.Range(0, cards.Length);
+            GameObject card = Instantiate(cards[rnd], new Vector3(xWidth - (i * xWidth), 0, 0), Quaternion.identity);
+            card.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+            activeCards.Add(card);
+        }
+    }
+
+    public void CardPicked(int index)
+    {
+        Debug.Log("CardPicked");
+        deck.Add(index);
+        foreach (GameObject g in activeCards)
+        {
+            Destroy(g);
+        }
+        canChoose = true;
+        choiceCount--;
+    }
+}
---
+++ Assets/CardSelected.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardSelected : MonoBehaviour
+{
+    public int index;
+    CardPick cardPick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cardPick = GameObject.Find("CardManager").GetComponent<CardPick>();
+        this.GetComponent<Button>().onClick.AddListener(PickCard);
+    }
+    void PickCard()
+    {
+        cardPick.CardPicked(index);
+    }
+}
